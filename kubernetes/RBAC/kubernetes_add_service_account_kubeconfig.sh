a25b50028beaee693f7ec8e13c886748b7adeb70
wwyhy <wwyhy520@163.com>
2024-04-28 18:38:32 +0800

    Update kubernetes_add_service_account_kubeconfig.sh

--- kubernetes/RBAC/kubernetes_add_service_account_kubeconfig.sh
+++ kubernetes/RBAC/kubernetes_add_service_account_kubeconfig.sh
@@ -3,19 +3,21 @@ set -e
 set -o pipefail
 
 # https://gist.github.com/sdarwin/ffc5fed82b6128a7549fd6d5696d98a8
-#
 # Based on https://gist.github.com/innovia/fbba8259042f71db98ea8d4ad19bd708
+
+set -e
+set -o pipefail
 
 # Add user to k8s using service account, no RBAC (must create RBAC after this script)
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
- echo "usage: $0 <service_account_name> <namespace>"
+ echo "usage: $0 <namespace> <service_account_name>"
  exit 1
 fi
 
-SERVICE_ACCOUNT_NAME=$1
-NAMESPACE="$2"
-KUBECFG_FILE_NAME="/tmp/kube/k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
-TARGET_FOLDER="/tmp/kube"
+SERVICE_ACCOUNT_NAME=$2
+NAMESPACE="$1"
+KUBECFG_FILE_NAME="./tmp/kube/k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
+TARGET_FOLDER="./tmp/kube"
 
 create_target_folder() {
     echo -n "Creating target directory to hold files in ${TARGET_FOLDER}..."
@@ -23,53 +25,22 @@ create_target_folder() {
     printf "done"
 }
 
-create_service_account() {
-    echo -e "\\nCreating a service account in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}"
-    kubectl apply -f - <<EOF
-    apiVersion: v1
-    kind: ServiceAccount
-    metadata:
-      name: ${SERVICE_ACCOUNT_NAME}
-      namespace: ${NAMESPACE}
-EOF
-}
-
-create_service_account_secret() {
-    echo -e "\\nCreating a service account secret in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}-secret"
-    kubectl apply -f - <<EOF
-    apiVersion: v1
-    kind: Secret
-    metadata:
-      name: $SERVICE_ACCOUNT_NAME-secret
-      namespace: ${NAMESPACE}
-      annotations:
-        kubernetes.io/service-account.name: $SERVICE_ACCOUNT_NAME
-    type: kubernetes.io/service-account-token
-EOF
-}
-
-get_secret_name_from_service_account_legacy() {
-    echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=$(kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o json | jq -r .secrets[].name)
-    echo "Secret name: ${SECRET_NAME}"
-}
-
 get_secret_name_from_service_account() {
     echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=${SERVICE_ACCOUNT_NAME}-secret
+    SECRET_NAME=$(kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o json | jq -r .secrets[].name)
     echo "Secret name: ${SECRET_NAME}"
 }
 
 extract_ca_crt_from_secret() {
     echo -e -n "\\nExtracting ca.crt from secret..."
     kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq \
-    -r '.data["ca.crt"]' | base64 --decode > "${TARGET_FOLDER}/ca.crt"
+    -r '.data["ca.crt"]' | base64 -d > "${TARGET_FOLDER}/ca.crt"
     printf "done"
 }
 
 get_user_token_from_secret() {
     echo -e -n "\\nGetting user token from secret..."
-    USER_TOKEN=$(kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data["token"]' | base64 --decode)
+    USER_TOKEN=$(kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data["token"]' | base64 -d)
     printf "done"
 }
 
@@ -112,31 +83,11 @@ set_kube_config_values() {
     --kubeconfig="${KUBECFG_FILE_NAME}"
 }
 
-create_rbac() {
-    kubectl apply --namespace=$NAMESPACE -f - <<EOF
-        kind: ClusterRoleBinding
-        apiVersion: rbac.authorization.k8s.io/v1
-        metadata:
-          name: ${SERVICE_ACCOUNT_NAME}-clusterrolebinding
-        subjects:
-          - kind: ServiceAccount
-            name: ${SERVICE_ACCOUNT_NAME}
-            namespace: ${NAMESPACE}
-        roleRef:
-          kind: ClusterRole
-          name: cluster-admin
-          apiGroup: ""
-EOF
-}
-
 create_target_folder
-create_service_account
-create_service_account_secret
 get_secret_name_from_service_account
 extract_ca_crt_from_secret
 get_user_token_from_secret
 set_kube_config_values
-create_rbac
 
 echo -e "\\nAll done! Test with:"
 echo "KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods"
